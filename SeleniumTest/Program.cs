1d2a0d5d3a07a9b03f12aa488f208ad3715a250b
Ian Edgeley <ianedgeley@gmail.com>
2020-10-23 10:45:37 +0100

    Simple google search, pulling the top result

--- SeleniumTest/Program.cs
+++ SeleniumTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTest
 {
@@ -12,6 +13,20 @@ namespace SeleniumTest
             // Load up Google and print out the URL
             driver.Navigate().GoToUrl(@"https://www.google.com");
             Console.WriteLine(driver.Url);
+            // Clear the cookie popup
+            driver.SwitchTo().Frame(0);
+            IWebElement popup = driver.FindElement(By.CssSelector("#introAgreeButton"));
+            popup.Click();
+            // Find the text input 
+            driver.SwitchTo().DefaultContent();
+            IWebElement searchbox = driver.FindElement(By.CssSelector("[title^='Search']"));
+            searchbox.SendKeys("hello world" + Keys.Enter);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement firstResult = wait.Until(e => e.FindElement(By.XPath("//a/h3")));
+
+            Console.WriteLine(firstResult.Text);
+            Console.ReadLine();
+
         }
     }
 }
