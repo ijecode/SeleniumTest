92d7aca53ad74b1d3fd3c82f8f9cfe89457f31d7
Ian Edgeley <ianedgeley@gmail.com>
2020-10-23 09:50:39 +0100

    First commit

---
+++ SeleniumTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver("C:/WebDriver/bin");
+            // Load up Google and print out the URL
+            driver.Navigate().GoToUrl(@"https://www.google.com");
+            Console.WriteLine(driver.Url);
+        }
+    }
+}
